b16eead97e06977e07bd966643148eae8ed4fdf7
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-03-28 15:46:29 -0400

    see line 9

--- reverser.py
+++ reverser.py
@@ -1,17 +1,18 @@
 def countLeadingZeros(aFloat, length):
     import math
     
+    aFloat = math.modf(aFloat)[1]
     if (math.modf(aFloat)[1] != 0) or (length == 1):
         aCounter = -1 
         return aCounter
 
+    #gives all correct answers but line 10 still gives infinite loop 
     else:
         aCounter = countLeadingZeros(aFloat * 10, length)
         aCounter += 1
     return aCounter
     
 def reverse(aNumber):
-    print("aNumber is ", aNumber)
     
     """Returns aNumber with all digits reversed. Assume positive aNumber."""
 
@@ -20,36 +21,24 @@ def reverse(aNumber):
     while getANumberLength > 10 :
         getANumberLength  = getANumberLength / 10
         length += 1
-    
-    print("length is ", length)
 
-    if length < 1:    #the real length is  = 1 but aNumber never goes into the 
-                      # while, so the variable 'length' never gets incremented
-        print("base case returns: ", aNumber)
+    if length < 1: 
         return aNumber
 
     import math
 
     firstDigit = math.modf(aNumber / (10**length))[1]
-    print("firstDigit is " , firstDigit)
     decimalPart = math.modf(aNumber / (10**length))[0]
-    print("decimalPart is ", decimalPart)
 
     zerosToAdd = 0
     if (decimalPart * 10) < 1:
         zerosToAdd = countLeadingZeros(decimalPart, length)
-        print ("firstDigit is", firstDigit)
 
     returnedNumber = reverse(round(decimalPart, length)* (10**(length)))
     if zerosToAdd != 0:
         returnedNumber = (returnedNumber * (10**zerosToAdd))
-    print("retirned number is ", returnedNumber)
 
     return ((returnedNumber * 10) +firstDigit)
-
 #print(reverse(1234))#, 4321)
 #print(reverse(12045))#, 78901)
 print(reverse(1020))#, 201)
-
-"""     if zerosToAdd != 0:
-        returnedNumber * (10**zerosToAdd) """
