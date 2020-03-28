89b6d30081921f2d049fe5e734c8a9ce5e3dde76
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-03-28 09:50:18 -0400

    reverser runs for 1234

--- reverser.py
+++ reverser.py
@@ -1,21 +1,31 @@
 def reverse(aNumber):
-    #divide by 10
+    
     """Returns aNumber with all digits reversed. Assume positive aNumber."""
 
     #get the length of aNumber without using len()
     getANumberLength = aNumber
-    aCounter = 0
+    length = 0
     while getANumberLength > 10 :
-        getANumberLength/10
-        aCounter++
-    print("aCounter is ", aCounter)
+        getANumberLength  = getANumberLength / 10
+        length += 1
 
-    #get the modulo of aNumber using a exponent of temn to get a number between 1 and 10
+    # when reverser is called with a one digit number return it  #base case
+    if length < 1:    #the real length is  = 1 but aNumber never goes into the 
+                      # while so the variable 'length' never gets incremented
+        return aNumber
+
+    #get the modulo of aNumber using an exponent of ten to get a number between 1 and 10
     # use modf to get the integer and decimal parts
+    import math
+    firstDigit = math.modf(aNumber / (10**length))[1]
+    decimalPart = math.modf(aNumber / (10**length))[0]
+
     # save the int for when the recursive call of reverser returns
     # return the decimal part of modf to an int
     # call reverser with this int
-    # when reverser is called with a one digit number return it  #base case
+    returnedNumber = reverse(round(decimalPart, length)* (10**(length)))
+    return ((returnedNumber * 10) +firstDigit)
+    
     # multiply the returned number by ten and add the saved int from the beginning of the recursive call/frame
     # return this number
 
@@ -23,7 +33,7 @@ def reverse(aNumber):
 print(reverse(1234))
 #, 4321)
 
-print(reverse(10987))#, 78901)
+#print(reverse(10987))#, 78901)
 #print(reverse(1020))#, 201)
 
 """
