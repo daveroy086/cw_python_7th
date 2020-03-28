e1384b0e9448fb9a86c8fa1b9e93e05476b14876
Dave Roy <51098204+daveroy086@users.noreply.github.com>
2020-03-28 15:29:19 -0400

    does not run for 1020. deleted some comments

--- reverser.py
+++ reverser.py
@@ -15,7 +15,6 @@ def reverse(aNumber):
     
     """Returns aNumber with all digits reversed. Assume positive aNumber."""
 
-    #get the length of aNumber without using len()
     getANumberLength = aNumber
     length = 0
     while getANumberLength > 10 :
@@ -24,14 +23,11 @@ def reverse(aNumber):
     
     print("length is ", length)
 
-    # when reverser is called with a one digit number return it  #base case
     if length < 1:    #the real length is  = 1 but aNumber never goes into the 
                       # while, so the variable 'length' never gets incremented
         print("base case returns: ", aNumber)
         return aNumber
 
-    #get the modulo of aNumber using an exponent of ten to get a number between 1 and 10
-    # use modf to get the integer and decimal parts
     import math
 
     firstDigit = math.modf(aNumber / (10**length))[1]
@@ -39,30 +35,21 @@ def reverse(aNumber):
     decimalPart = math.modf(aNumber / (10**length))[0]
     print("decimalPart is ", decimalPart)
 
-    #I need a loop that detects trialing zeros and adds them to firstDigit
-    #before reverse is called
     zerosToAdd = 0
     if (decimalPart * 10) < 1:
         zerosToAdd = countLeadingZeros(decimalPart)
         print ("firstDigit is", firstDigit)
 
-    # save the int for when the recursive call of reverser returns
-    # return the decimal part of modf to an int
-    # call reverser with this int
     returnedNumber = reverse(round(decimalPart, length)* (10**(length)))
     if zerosToAdd != 0:
         returnedNumber = (returnedNumber * (10**zerosToAdd))
     print("retirned number is ", returnedNumber)
 
     return ((returnedNumber * 10) +firstDigit)
-    
-    # multiply the returned number by ten and add the saved int from the beginning of the recursive call/frame
-    # return this number
 
-
-print(reverse(1234))#, 4321)
-print(reverse(12045))#, 78901)
-#print(reverse(1020))#, 201)
+#print(reverse(1234))#, 4321)
+#print(reverse(12045))#, 78901)
+print(reverse(1020))#, 201)
 
 """     if zerosToAdd != 0:
         returnedNumber * (10**zerosToAdd) """
